7b72f4a011caa741d238af35b41dbda10592e04a
unknown <you@example.com>
2020-12-09 14:15:45 +0700

    vuong commplete task statistical-device

--- src/dao/ConnectionDB.java
+++ src/dao/ConnectionDB.java
@@ -1,8 +1,13 @@
 package dao;
 
+
+import dao.impl.DeviceDAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
+import model.Device;
+
 
 public class ConnectionDB {
     public static void main(String[] args) {
@@ -10,12 +15,19 @@ public class ConnectionDB {
         try {
             String url = "jdbc:mysql://localhost:3306/qlnvh";
             String user = "root";
-            String password = "uchihamadara28@";
+            String password = "";
             Connection connection = DriverManager.getConnection(url, user, password);
             if (connection != null) {
                 System.out.println("hello world");
             }
-
+            
+           IDeviceDAO deviceDAO = new DeviceDAO();
+           List<Device> list = deviceDAO.findAll();
+           for(Device de : list){
+               System.out.println(de.getName());
+               System.out.println(de.getPrice());
+           }
+           
         } catch (SQLException e) {
             throw new Error("Problem", e);
         } finally {
--- src/quanlynhavanhoa/Quanlynhavanhoa.java
+++ src/quanlynhavanhoa/Quanlynhavanhoa.java
@@ -5,6 +5,7 @@
  */
 package quanlynhavanhoa;
 
+import java.sql.Connection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -20,12 +21,12 @@ public class Quanlynhavanhoa extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root;
-        root = FXMLLoader.load(getClass().getResource("/layout/register-form.fxml"));
-        primaryStage.setTitle("Biểu mẫu đăng ký");
+        root = FXMLLoader.load(getClass().getResource("/layout/statistical.fxml"));
+        primaryStage.setTitle("Thống kê");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
+    
     /**
      * @param args the command line arguments
      */
