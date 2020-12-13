26f656c8b574a657d5bdd4c24d07a96ce9b33627
Duy Do <dotheduybk131@gmail.com>
2020-12-14 01:21:11 +0700

    duy do model device

--- src/model/Device.java
+++ src/model/Device.java
@@ -1,40 +1,39 @@
-package model;
+        package model;
 
-public class Device {
-    private Integer id;
-    private String name;
-    private Long price;
+        public class Device {
+            private Integer id;
+            private String name;
+            private Long price;
+            public Device() {
+            }
 
-    public Device() {
-    }
+            public Device(Integer id, String name, Long price) {
+                this.id = id;
+                this.name = name;
+                this.price = price;
+            }
 
-    public Device(Integer id, String name, Long price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
+            public Integer getId() {
+                return id;
+            }
 
-    public Integer getId() {
-        return id;
-    }
+            public void setId(Integer id) {
+                this.id = id;
+            }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+            public String getName() {
+                return name;
+            }
 
-    public String getName() {
-        return name;
-    }
+            public void setName(String name) {
+                this.name = name;
+            }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+            public Long getPrice() {
+                return price;
+            }
 
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-}
+            public void setPrice(Long price) {
+                this.price = price;
+            }
+        }
